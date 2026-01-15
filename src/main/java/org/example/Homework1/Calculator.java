bca2d271f041681cc161391b3ab72fc87fe128e7
Theam Sreyphea <sonanhout17@gmail.com>
2026-01-15 13:16:51 +0700

    myhomework

---
+++ src/main/java/org/example/Homework1/Calculator.java
@@ -0,0 +1,19 @@
+package org.example.Homework1;
+
+public class Calculator {
+    static void main() {
+        int a = 10;
+        int b = 30;
+
+        int total = a + b;
+        int subtotal = b - a;
+        int mult = a * b;
+        int div = b/a;
+
+        System.out.println("Total = " + total);
+        System.out.println("Subtotal = " + subtotal);
+        System.out.println("Mult = " + mult);
+        System.out.println("Div = " + div);
+
+    }
+}
