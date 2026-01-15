8f4fd6e1b77764bb8248a6aec9e6ea84e4c0576e
Theam Sreyphea <sonanhout17@gmail.com>
2026-01-15 13:05:40 +0700

    my homework

--- src/main/java/org/example/Homework1/Varible_Practice.java
+++ src/main/java/org/example/Homework1/Varible_Practice.java
@@ -1,7 +1,25 @@
 package org.example.Homework1;
-
+import java.util.Scanner;
 public class Varible_Practice {
     static void main() {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please input name: ");
+      String studentName = sc.next();       System.out.print("please input Age: ");
+       Integer studentAge = sc.nextInt();
+
+       System.out.print("please input height: ");
+       Boolean studentHeight = sc.hasNext();
+
+        System.out.print("please input place is student: ");
+       Boolean placeIsStudent= sc.hasNextBoolean();
+
+        System.out.println("Student name = " + studentName);
+        System.out.println("Student Age = " + studentAge);
+        System.out.println("Student height = " + studentHeight);
+        System.out.println("Student place is student = " + placeIsStudent);
+
+
+        
     }
 }
